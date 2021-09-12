1d55e6837388186399e2d99f233bea6494e3b3af
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-09-13 00:34:37 +0600

    id added in the JwtPayload

--- src/users/interfaces/jwt-payload.interface.ts
+++ src/users/interfaces/jwt-payload.interface.ts
@@ -1,6 +1,7 @@
 export interface JwtPayload {
-    username: string;
-    email: string;
-    roles: string[];
-    displayName: string;
+  id?: string;
+  username: string;
+  email: string;
+  roles: string[];
+  displayName: string;
 }
